2afd27dc81e66365cf65c6dccb7654e49a1117c8
venkataamazon06 <venkataamazon06@gmail.com>
2025-05-11 12:11:51 +0100

    dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -1,13 +1,31 @@
-Fresh_Vegetables=[
+Fresh_Vegetables= [
     {
-        'Name': 'Mango Banganapalli'
-        'weight' : 1 'kg'
-        'price' : 181 'rs'
-        'image url' : 'https://cdn.zeptonow.com/production/ik-seo/tr:w-350,ar-3000-3000,pr-true,f-auto,q-80/cms/product_variant/9c45acfa-024c-4400-b46e-098580358421/Mango-Banganapalli.jpeg'
+        'Name': 'Mango Banganapalli',
+        'weight' : '1kg',
+        'price' : 181 ,
+        'image url' : 'https://cdn.zeptonow.com/production/ik-seo/tr:w-350,ar-3000-3000,pr-true,f-auto,q-80/cms/product_variant/9c45acfa-024c-4400-b46e-098580358421/Mango-Banganapalli.jpeg',
         'discount': 6
 
-    }
+    },
 
+    {
+        'Name': 'Tomato Local',
+        'weight' : '250g',
+        'price' : 16 ,
+        'image url' : 'https://cdn.zeptonow.com/production/ik-seo/tr:w-400,ar-3000-3000,pr-true,f-auto,q-80/cms/product_variant/98df9311-7dc6-44ab-92e6-fca8fdd4f23c/Tomato-Local.jpeg',
+        'discount': 6
+
+    },
+
+    {
+        'Name': 'Carrot Local',
+        'weight' : '500g',
+        'price' : 35 ,
+        'image url' : 'https://cdn.zeptonow.com/production/ik-seo/tr:w-400,ar-3000-3000,pr-true,f-auto,q-80/cms/product_variant/98df9311-7dc6-44ab-92e6-fca8fdd4f23c/Tomato-Local.jpeg',
+        'discount': 17
+
+    },
 
 
 ]
+print(Fresh_Vegetables)
