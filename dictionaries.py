67d72caabc775e4123ded49ba7eda398f13eebac
venkataamazon06 <venkataamazon06@gmail.com>
2025-05-11 12:11:22 +0100

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,13 @@
+Fresh_Vegetables=[
+    {
+        'Name': 'Mango Banganapalli'
+        'weight' : 1 'kg'
+        'price' : 181 'rs'
+        'image url' : 'https://cdn.zeptonow.com/production/ik-seo/tr:w-350,ar-3000-3000,pr-true,f-auto,q-80/cms/product_variant/9c45acfa-024c-4400-b46e-098580358421/Mango-Banganapalli.jpeg'
+        'discount': 6
+
+    }
+
+
+
+]
