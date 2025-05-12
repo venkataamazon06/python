6d785ea8200e3fb4521acdda2b0c34d65404e57b
venkataamazon06 <venkataamazon06@gmail.com>
2025-05-12 16:27:34 +0100

    complex dictionaries

---
+++ complex dictionaries.py
@@ -0,0 +1,25 @@
+filters= {
+    'Categories': {
+        'T-shirts': 220006,
+        'Lounge T-shirts':1033
+    },
+    'Brand':{
+        'woostro':11629,
+        'Roadster':5900,
+        'Greylongg':5214,
+        'fabsteive':3488
+    },
+    'Price':{
+        'Min': 100,
+        'Max':10100
+    },
+    'Color':{
+        'Black':36153,
+        'white':6177,
+        'Blue':24138,
+        'Green':16205
+    },
+    'Discount_Range':{"10% and above", "20% and above", "30% and above", "40% and above", "50% and above", "60% and above", "70% and above", "80% and above", "90% and above"}
+}
+
+print('discount range is :', filters['Discount_Range'])
